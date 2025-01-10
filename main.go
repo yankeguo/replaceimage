5592c2844dfb0aa13f0b0e4d96052a451fb0e8b2
GUO YANKE <hi@yankeguo.com>
2025-01-10 13:42:41 +0800

    feat: handle the case like docker.io/[REPO]

--- main.go
+++ main.go
@@ -42,15 +42,21 @@ func standardizeImage(image string) string {
 
 	splits := strings.Split(image, "/")
 
-	if len(splits) > 1 {
-		if strings.Contains(splits[0], ".") || strings.Contains(splits[0], ":") {
-			return image
-		} else {
-			return "docker.io/" + image
-		}
+	// single part
+	if len(splits) < 2 {
+		return "docker.io/library/" + image
 	}
 
-	return "docker.io/library/" + image
+	// custom domain
+	if strings.Contains(splits[0], ".") || strings.Contains(splits[0], ":") {
+		// first part is docker.io and only 2 parts
+		if len(splits) == 2 && splits[0] == "docker.io" {
+			return "docker.io/library/" + splits[1]
+		}
+		return image
+	}
+
+	return "docker.io/" + image
 }
 
 func main() {
--- main_test.go
+++ main_test.go
@@ -9,5 +9,6 @@ import (
 func TestStandardizeImage(t *testing.T) {
 	require.Equal(t, "docker.io/library/nginx:latest", standardizeImage("nginx"))
 	require.Equal(t, "docker.io/yankeguo/minit:latest", standardizeImage("yankeguo/minit"))
+	require.Equal(t, "docker.io/library/alpine:3.18", standardizeImage("docker.io/alpine:3.18"))
 	require.Equal(t, "gcr.io/hello:v2", standardizeImage("gcr.io/hello:v2"))
 }
